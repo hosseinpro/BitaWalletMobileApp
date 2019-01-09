9fe687597f92b30cee5337d0c831c0c090692cf8
hosseinpro <hossein.rezaei.ghaleh@gmail.com>
2019-01-09 14:42:21 -0500

    add cardBootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import NfcManager, { NfcTech } from "react-native-nfc-manager";
-import BitaWalletCard from "./src/BitaWalletCard";
+import BitaWalletCard from "./BitaWalletCard";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       supported: false,
-      enabled: false,
-      isTestRunning: false,
-      tag: null,
       bitaWalletCard: null
     };
   }
@@ -21,7 +18,6 @@ export default class App extends Component {
       if (supported) {
         const bitaWalletCard = new BitaWalletCard(this.transmit.bind(this));
         this.setState({ bitaWalletCard });
-        this._startNfc();
       }
     });
   }
@@ -29,7 +25,7 @@ export default class App extends Component {
   transmit(commandAPDU) {
     return new Promise((resolve, reject) => {
       commandAPDU = commandAPDU.toUpperCase().replace(/\s/g, "");
-      // is connected
+      // is connected?
       let inBuffer = BitaWalletCard.hex2Bytes(commandAPDU);
       NfcManager.transceive(inBuffer)
         .then(outBuffer => {
@@ -42,13 +38,16 @@ export default class App extends Component {
     });
   }
 
-  _startNfc = () => {
+  cardDetection = eventFunction => {
     NfcManager.start()
       .then(() => NfcManager.isEnabled())
+      .then(() => NfcManager.closeTechnology())
+      .then(() => NfcManager.unregisterTagEvent())
       .then(() => NfcManager.registerTagEvent())
       .then(() => NfcManager.requestTechnology(NfcTech.IsoDep))
       .then(() => NfcManager.getTag())
-      .then(() => this.test())
+      .then(() => this.cardBootstrap())
+      .then(() => eventFunction())
       .then(() => NfcManager.closeTechnology())
       .then(() => NfcManager.unregisterTagEvent())
       .catch(err => {
@@ -57,10 +56,36 @@ export default class App extends Component {
       });
   };
 
-  test() {
+  cardBootstrap() {
+    let cardInfo = {};
     this.state.bitaWalletCard
       .selectApplet()
-      .then(() => console.log("Applet selected"))
+      .then(
+        this.state.bitaWalletCard
+          .getSerialNumber()
+          .then(res => (cardInfo.serialNumber = res.serialNumber))
+          .then(
+            this.state.bitaWalletCard
+              .getVersion()
+              .then(res => {
+                cardInfo.type = res.type;
+                cardInfo.version = res.version;
+              })
+              .then(
+                this.state.bitaWalletCard
+                  .getLabel()
+                  .then(res => (cardInfo.label = res.label))
+                  .then(() => console.log(cardInfo))
+              )
+          )
+      )
+      .catch(error => console.log(error));
+  }
+
+  cardVerifyPIN() {
+    this.state.bitaWalletCard
+      .verifyPIN("1234")
+      .then(() => console.log("PIN verified"))
       .catch(error => console.log("Error:" + error.message));
   }
 
@@ -68,7 +93,11 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>My first app!</Text>
-        <Button onPress={() => this._startNfc()} title="Test" color="#841584" />
+        <Button
+          onPress={() => this.cardDetection(this.cardVerifyPIN.bind(this))}
+          title="cardVerifyPIN"
+          color="#841584"
+        />
       </View>
     );
   }
