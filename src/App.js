00453390e49f086e06cc35a62619c45bebd0cb7a
hosseinpro <hossein.rezaei.ghaleh@gmail.com>
2019-01-16 08:18:51 -0500

    separate NfcReader module.

--- src/App.js
+++ src/App.js
@@ -1,103 +1,52 @@
 import React, { Component } from "react";
 import { StackNavigator } from "react-navigation";
-import { StyleSheet, Text, View } from "react-native";
-import NfcManager, { NfcTech } from "react-native-nfc-manager";
-import BitaWalletCard from "./BitaWalletCard";
+import { StyleSheet, Text, View, Button } from "react-native";
+import NfcReader from "./lib/NfcReader";
 import MainScreen from "./components/MainScreen";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      supported: false,
-      bitaWalletCard: null,
-      cardInfo: {}
+      nfcReader: null,
+      supported: false
     };
   }
 
   componentDidMount() {
-    NfcManager.isSupported().then(supported => {
+    const nfcReader = new NfcReader();
+    nfcReader.isSupported().then(supported => {
       this.setState({ supported });
       if (supported) {
-        const bitaWalletCard = new BitaWalletCard(this.transmit.bind(this));
-        this.setState({ bitaWalletCard });
+        this.setState({ nfcReader });
       }
     });
   }
 
-  transmit(commandAPDU) {
-    return new Promise((resolve, reject) => {
-      commandAPDU = commandAPDU.toUpperCase().replace(/\s/g, "");
-      // is connected?
-      let inBuffer = BitaWalletCard.hex2Bytes(commandAPDU);
-      NfcManager.transceive(inBuffer)
-        .then(outBuffer => {
-          const responseAPDU = BitaWalletCard.bytes2Hex(outBuffer);
-          resolve(responseAPDU);
-        })
-        .catch(error => {
-          reject(error);
-        });
-    });
-  }
-
-  cardDetection = eventFunction => {
-    NfcManager.start()
-      .then(() => NfcManager.isEnabled())
-      .then(() => NfcManager.closeTechnology())
-      .then(() => NfcManager.unregisterTagEvent())
-      .then(() => NfcManager.registerTagEvent())
-      .then(() => NfcManager.requestTechnology(NfcTech.IsoDep))
-      .then(() => NfcManager.getTag())
-      .then(() => this.getCardInfo())
-      .then(() => eventFunction())
-      .then(() => NfcManager.closeTechnology())
-      .then(() => NfcManager.unregisterTagEvent())
-      .catch(err => {
-        console.warn(err);
-        this.setState({ enabled: false });
-      });
-  };
-
-  getCardInfo() {
-    let cardInfo = {};
-    this.state.bitaWalletCard
-      .selectApplet()
-      .then(
-        this.state.bitaWalletCard
-          .getSerialNumber()
-          .then(res => (cardInfo.serialNumber = res.serialNumber))
-          .then(
-            this.state.bitaWalletCard
-              .getVersion()
-              .then(res => {
-                cardInfo.type = res.type;
-                cardInfo.version = res.version;
-              })
-              .then(
-                this.state.bitaWalletCard
-                  .getLabel()
-                  .then(res => (cardInfo.label = res.label))
-                  .then(() => this.setState({ cardInfo }))
-              )
-          )
-      )
-      .catch(error => console.log(error));
-  }
-
   cardVerifyPIN() {
-    this.state.bitaWalletCard
+    this.state.nfcReader.bitaWalletCard
       .verifyPIN("1234")
       .then(() => {
         console.log("PIN verified");
-        console.log(this.state.cardInfo);
+        console.log(this.state.nfcReader.cardInfo);
       })
       .catch(error => console.log("Error:" + error.message));
   }
 
   render() {
-    return <AppStackNavigator />;
-    // return <View />;
+    // return <AppStackNavigator />;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>My first app!</Text>
+        <Button
+          onPress={() =>
+            this.state.nfcReader.cardDetection(this.cardVerifyPIN.bind(this))
+          }
+          title="cardVerifyPIN"
+          color="#841584"
+        />
+      </View>
+    );
   }
 }
 
---
+++ src/lib/NfcReader.js
@@ -0,0 +1,86 @@
+"use strict";
+
+import NfcManager, { NfcTech } from "react-native-nfc-manager";
+import BitaWalletCard from "./BitaWalletCard";
+
+module.exports = class NfcReader {
+  isSupported() {
+    return new Promise((resolve, reject) => {
+      NfcManager.isSupported()
+        .then(supported => {
+          if (supported) {
+            this.bitaWalletCard = new BitaWalletCard(this.transmit.bind(this));
+          }
+          resolve(supported);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+
+  transmit(commandAPDU) {
+    return new Promise((resolve, reject) => {
+      commandAPDU = commandAPDU.toUpperCase().replace(/\s/g, "");
+      // is connected?
+      let inBuffer = BitaWalletCard.hex2Bytes(commandAPDU);
+      NfcManager.transceive(inBuffer)
+        .then(outBuffer => {
+          const responseAPDU = BitaWalletCard.bytes2Hex(outBuffer);
+          resolve(responseAPDU);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+
+  cardDetection = eventFunction => {
+    NfcManager.start()
+      .then(() => NfcManager.isEnabled())
+      .then(() => NfcManager.closeTechnology())
+      .then(() => NfcManager.unregisterTagEvent())
+      .then(() => NfcManager.registerTagEvent())
+      .then(() => NfcManager.requestTechnology(NfcTech.IsoDep))
+      .then(() => NfcManager.getTag())
+      .then(() => this.getCardInfo())
+      .then(() => eventFunction())
+      .then(() => NfcManager.closeTechnology())
+      .then(() => NfcManager.unregisterTagEvent())
+      .catch(err => {
+        console.warn(err);
+        this.setState({ enabled: false });
+      });
+  };
+
+  getCardInfo() {
+    return new Promise((resolve, reject) => {
+      let cardInfo = {};
+      this.bitaWalletCard
+        .selectApplet()
+        .then(
+          this.bitaWalletCard
+            .getSerialNumber()
+            .then(res => (cardInfo.serialNumber = res.serialNumber))
+            .then(
+              this.bitaWalletCard
+                .getVersion()
+                .then(res => {
+                  cardInfo.type = res.type;
+                  cardInfo.version = res.version;
+                })
+                .then(
+                  this.bitaWalletCard
+                    .getLabel()
+                    .then(res => (cardInfo.label = res.label))
+                    .then(() => (this.cardInfo = cardInfo))
+                    .then(() => resolve(cardInfo))
+                )
+            )
+        )
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
+};
