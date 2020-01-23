ed708e5cdd4c2f22a57b8192c35fa2dc58d29bc8
hosseinpro <hossein.rezaei.ghaleh@gmail.com>
2020-01-23 10:32:04 -0500

    Add iOS native module

---
+++ ios/NfcModule.m
@@ -0,0 +1,9 @@
+//
+//  NfcModule.m
+//  BitaWalletMobileApp
+//
+//  Created by Hossein Rezaeighaleh on 1/22/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/NfcModule.swift
@@ -0,0 +1,9 @@
+//
+//  NfcModule.swift
+//  BitaWalletMobileApp
+//
+//  Created by Hossein Rezaeighaleh on 1/22/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
